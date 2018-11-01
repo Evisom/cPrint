849b3a53fa23344e048f17a713a667bf1dc9d7a1
Dmitriy Shishmintsev <chesminsky@icloud.com>
2018-11-01 15:47:46 +0300

    colors

---
+++ README.md
@@ -0,0 +1,27 @@
+![alt text](https://i.imgur.com/dvXzgOP.png "")
+
+# How to use?
+
+### 1. Installation
+``` 
+~$ npm i cprint
+```
+
+### 2. Connect cPrint
+```
+const cPrint = require('cprint');
+```
+
+### 3. Use!
+```
+cPrint( 'text', 'color' );
+```
+
+## Text
+### A-Z, a-z
+
+## Colors
+
+![alt text](https://i.imgur.com/V3FZtuD.png "")
+
+
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const letters = {
     a: [
         '     ',
@@ -238,18 +240,26 @@ const letters = {
     space: ['  ', '  ', '  ', '  ', '  ','  ','  '],
 }
 
+const colors = {
+    'Reset' : "\x1b[0m",
+    'black' : "\x1b[30m",
+    'red' : "\x1b[31m",
+    'green' : "\x1b[32m",
+    'yellow' : "\x1b[33m",
+    'blue' : "\x1b[34m",
+    'magenta' : "\x1b[35m",
+    'cyan' : "\x1b[36m",
+    'white' : "\x1b[37m",
+}
+let colors_name = Object.keys(colors);
+colors_name.shift()
 
 
-
-
-
-
-
-
-module.exports = function main(text) {
-
+// module.exports =
+ function main(text , color) {
+    color = color.toLowerCase();
     function render(letter, line) {
-
+        let nletter;
         // console.log(letter);
         if (letter === ' ') {
             nletter = letters.space;
@@ -261,8 +271,21 @@ module.exports = function main(text) {
     }
 
     function print() {
+        let ncolor;
+        let random_color
+      
+        if (color == undefined || color == null || color == '') {
+            ncolor = colors['white'];
+        } else if (color == 'random') {
+            random_color = (Math.random() * (colors_name.length - 1) ).toFixed(0);
+            random_color = colors_name[random_color]
+            ncolor = colors[random_color]
+        } else {
+            ncolor = colors[color]
+        }
+
         for (let i = 0; i < 7; i++) {
-            console.log(result[i]);
+            console.log(ncolor, result[i] , colors.Reset);
         }
     }
 
@@ -276,6 +299,6 @@ module.exports = function main(text) {
     }
 
     print();
-
 }
 
+main ('erwrwer' , 'bLaCK');
